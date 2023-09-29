ae5d690aba544d09fe1cbc74be90626063a01194
chithirai jothi <80042470+chithiraijothi07@users.noreply.github.com>
2023-09-29 19:16:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ db_databricks = SQLDatabase.from_databricks(
 )
 
 # Set up the OpenAI instance
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = "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"
 llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo-16k', openai_api_key=OPENAI_API_KEY)
 
 # Set up the SQLDatabaseChain
